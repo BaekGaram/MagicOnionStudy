43e968df7b68f017881808397aa0ca9c9a2d3da6
BaekGaram <bgr1017@actgames.co.kr>
2024-09-27 18:55:55 +0900

    버튼 클릭시 player 추가 되게 수정

--- MyProjectClient/Assets/Scripts/UiManager.cs
+++ MyProjectClient/Assets/Scripts/UiManager.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class UiManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerObject;
-    
     private bool _isConnected = false;
+    
+    public GameObject cubePrefab; // 생성할 큐브 프리팹
 
     // Start is called before the first frame update
     void Start()
@@ -28,5 +28,21 @@ public class UiManager : MonoBehaviour
         _isConnected = true;
         
         Debug.LogError("Connected to MagicOnion Hub");
+    }
+
+    public async void CreatePlayer()
+    {
+        // 큐브를 생성할 위치를 현재 위치 기준으로 설정 (필요에 따라 수정 가능)
+        Vector3 spawnPosition = new Vector3(0, 0, 0); 
+
+        // 큐브 오브젝트를 생성 (Instantiate)
+        GameObject newCube = Instantiate(cubePrefab, spawnPosition, Quaternion.identity);
+        
+        // 생성된 큐브의 이름을 "New Cube"로 설정
+        newCube.name = "New Cube";
+        newCube.SetActive(true);
+
+        // 콘솔에 메시지 출력
+        Debug.Log("Cube created at position: " + spawnPosition);
     }
 }
